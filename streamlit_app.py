0ca3045fea5b41950e7840d00033c0a840c50d4c
Dev Sharma <48862834+b4batunde@users.noreply.github.com>
2025-05-20 02:58:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_community.llms import OpenAI
 from langchain.chains import RetrievalQA
+from langchain_openai import OpenAIEmbeddings, OpenAI
 import os
 
 # Loading API key
